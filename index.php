16db043455511b68eec30eb6ad0be7661a93d6e1
stephbak <stephane.bakum@sfr.fr>
2018-12-14 16:02:34 +0100

    exercice4

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8" />
+  <title>php partie1</title>
+</head>
+<body>
+  <p>
+    <?php
+    $string = 'Bonjour!';
+    $int = 25;
+    $float = 85.8;
+    $imInFormation = true;
+    echo $string . ' ' . $int . ' ' . $float . ' ' . $imInFormation;
+    ?>
+  </p>
+</body>
+</html>
